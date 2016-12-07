81114b376e992677fafda130ca8087548f0c6e8d
mtorgovitsky <sisma123>
2016-12-07 15:41:24 +0200

    Already Better

---
+++ Task9/Modules/Items.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9.Modules
+{
+    public class Item
+    {
+        public string ProductName;
+        private readonly int ProductID;
+        public int Quantity;
+
+        static int CurrentID = 1;
+
+
+        public Item(string name, int quantity)
+        {
+            ProductName = name;
+            Quantity = quantity;
+            ProductID = CurrentID;
+            CurrentID++;
+        }
+
+        public class FoodItem : Item
+        {
+            public DateTime productionDate { get; set; }
+            public DateTime expiryDate { get; set; }
+
+            public FoodItem(string name, int quantity, DateTime prodDate, DateTime expDate)
+                : base(name, quantity)
+            {
+                productionDate = prodDate;
+                expiryDate = expDate;
+            }
+        }
+
+        //public class RafrigeratedItem : FoodItem
+        //{
+        //    public int minTemperature;
+        //    public int maxTemperature;
+
+        //    public RafrigeratedItem()
+        //    {
+
+        //    }
+        //}
+
+        public class ElectronicItem : Item
+        {
+            public DateTime guaranteeDate;
+
+            public ElectronicItem(string name, int quantity, DateTime guarDate)
+                :base(name, quantity)
+            {
+                guaranteeDate = guarDate;
+            }
+        }
+    }
+
+
+}
--- Task9/Program.cs
+++ Task9/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task9.Modules;
+using static Task9.Modules.Item;
 
 namespace Task9
 {
@@ -10,6 +12,8 @@ namespace Task9
     {
         static void Main(string[] args)
         {
+            Item it = new Item("GB", 3);
+            FoodItem fi = new FoodItem("HH", 4, DateTime.Now, DateTime.Now.AddYears(1));
         }
     }
 }
